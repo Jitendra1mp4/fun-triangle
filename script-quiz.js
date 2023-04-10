b5008a93715388723dede4360d2f051cf4079b56
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-10 21:29:29 +0530

    object of qns is created.

---
+++ script-quiz.js
@@ -0,0 +1,18 @@
+let score = 0;
+const questions = [
+  {
+    qn: "The sum of the length of any two sides of a triangle is always ______ than the third side.",
+    ans: "greater",
+    opt: ["smaller", "larger", "greater"],
+  },
+  {
+    qn: "How many vertices a triangle have?",
+    ans: "3",
+    opt: ["3", "90", "4"],
+  },
+  {
+    qn: "What is the sum of all three angles of a triangle?",
+    ans: "180 degree",
+    opt: ["90 degree", "180 degree", "360 degree"],
+  },
+];
