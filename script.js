b783e69a12e1a362823f298b4b09a4dba825eaba
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-10 17:51:32 +0530

    doc : comments added to functions.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// get required elements
 const inputAngels = document.getElementsByTagName("input");
 const btnCheck = document.getElementById("btn-check");
 const divOutput = document.getElementById("div-output");
 
+// addEventListener for check btn click
 btnCheck.addEventListener("click", () => {
+  // get sum of elements
   let sum = getSum();
   if (sum !== undefined) setMessage(sum);
 });
 
+// arrow function to calculate sum.
 const getSum = () => {
   let sum = 0;
   for (let i = 0; i < 3; i++) {
@@ -20,12 +24,13 @@ const getSum = () => {
   return sum;
 };
 
-function setMessage(sum) {
+// arrow function to set the message (result)
+const setMessage = (sum) => {
   let outputMessage;
   if (sum === 180)
     outputMessage =
-      " <strong>Yes!</strong> given angel can form a triangle.🎉️";
+      "<strong>Yes!</strong> given angel can form a triangle.🎉️";
   else
     outputMessage = "<strong>No!</strong> given angel can NOT form a triangle.";
   divOutput.innerHTML = outputMessage;
-}
+};
