dc46cc333fa21006b6684f8cba90feedf6a31a4c
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-04-12 08:46:49 +0530

    feat : now area page can calculate area.

--- script-area.js
+++ script-area.js
@@ -1,26 +1,25 @@
 // get required elements
-const inputBase = document.getElementById('base') ;
-const inputHeight = document.getElementById('height'); 
+const inputBase = document.getElementById("base");
+const inputHeight = document.getElementById("height");
 const btnGetArea = document.getElementById("btn-get-area");
 const divOutput = document.getElementById("div-output");
 
-// addEventListener for check btn click
+// addEventListener for btn-get-area click
 btnGetArea.addEventListener("click", () => {
   // get sum of elements
-  if(inputBase.value !== '' || inputBase.value !== '' ){
-      const area = getArea(inputBase.value,inputHeight.value);
-      console.log(area);
-      setMessage(area);
-  }else alert("Please enter the values and try again!");
+  if (inputBase.value !== "" || inputBase.value !== "") {
+    const area = getArea(inputBase.value, inputHeight.value);
+area    setMessage(area);
+  } else alert("Please enter the values and try again!");
 });
 
 // arrow function to calculate area.
-const getArea = (base,height) => {
-  return (base * height)/2;
+const getArea = (base, height) => {
+  return (base * height) / 2;
 };
 
 // arrow function to set the message (result)
-const setMessage = (hypo) => {
-  let outputMessage = `Area of Triangle = <strong>${hypo}</strong>`;
-    divOutput.innerHTML = outputMessage;
+const setMessage = (area) => {
+  let outputMessage = `Area of Triangle = <strong>${area}</strong>`;
+  divOutput.innerHTML = outputMessage;
 };
